603d3fadd0e2e1596b7cd1a34683b2c54282e784
Kelsey White <kl457563@dal.ca>
2025-11-17 09:53:44 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="container text-center">
+      <h1>Welcome to my Full Stack App</h1>
+      <p>This is a React app styled with bootstrap 5.</p>
+    </div>
+  );
+}
+
+export default App;
